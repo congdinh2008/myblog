50314bbb0737f3386fa69774eaefcd99eaacad25
congdinh <congdinh2012@hotmail.com>
2020-04-23 01:25:05 +0700

    Create base web project

---
+++ MyBlog/MyBlog.Presentation/Data/MyBlogDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyBlog.Presentation.Data
+{
+    public class MyBlogDbContext : IdentityDbContext
+    {
+        public MyBlogDbContext(DbContextOptions<MyBlogDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
